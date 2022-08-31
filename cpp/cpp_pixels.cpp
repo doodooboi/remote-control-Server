a45b0994937bcffc1fd0d0c5a647c729a1d4ca1f
ytugy <65300639+doodooboi@users.noreply.github.com>
2022-08-31 11:38:32 -0500

    Commit 2

---
+++ cpp/cpp_pixels.cpp
@@ -0,0 +1,21 @@
+#include <napi.h>
+#include <ImageMagik.h>
+
+using namespace Napi;
+
+Napi::Array:Array GetImagePixels(file) {
+    
+}
+
+Napi::String Method(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  return Napi::String::New(env, "world");
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  exports.Set(Napi::String::New(env, "HelloWorld"),
+              Napi::Function::New(env, Method));
+  return exports;
+}
+
+NODE_API_MODULE(addon, Init)
